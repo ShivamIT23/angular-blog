c3b8634e8c4aff4e61474f6357f96d7df1fa0523
ShivamIT23 <shivam.gupta.23112003@gmail.com>
2025-07-28 13:12:27 +0530

    Final version 1 Correct production backend url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  backendUrl: 'https://your-production-api-url.com' // Replace with your actual production API URL
+  backendUrl: 'http://ec2-13-127-217-70.ap-south-1.compute.amazonaws.com:8000' // Replace with your actual production API URL
 }; 
