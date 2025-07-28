97510321b194ea54489f80f55cd01abad2d96f1b
ShivamIT23 <shivam.gupta.23112003@gmail.com>
2025-07-28 14:32:33 +0530

    Changed Env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  backendUrl: 'http://ec2-13-127-217-70.ap-south-1.compute.amazonaws.com:8000' // Replace with your actual production API URL
+  backendUrl: 'https://www.blog.shivam23.me' // Replace with your actual production API URL
 }; 
