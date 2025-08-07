dd4db357bd7f4075509af9738cc730c9dd08ac7f
ShivamIT23 <shivam.gupta.23112003@gmail.com>
2025-08-07 12:54:04 +0530

    Updated the environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  backendUrl: 'https://python.acsend.tech/' // Replace with your actual production API URL
+  backendUrl: 'https://python.acsend.tech' // Replace with your actual production API URL
   // backendUrl: 'http://127.0.0.1:8000' // Replace with your actual production API URL
 }; 
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  backendUrl: 'https://python.acsend.tech/'
+  backendUrl: 'https://python.acsend.tech'
   // backendUrl: 'http://127.0.0.1:8000'
 };
